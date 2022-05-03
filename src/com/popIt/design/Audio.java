05528c347e851729788bccb3779c10aaec4a2f69
Jillian Sechrest <jillian.sechrest@bewaterwsnc.com>
2022-05-03 13:03:32 -0400

    THROW AWAY

---
+++ src/com/popIt/design/Audio.java
@@ -0,0 +1,47 @@
+package com.popIt.design;
+
+
+import java.io.File;
+import java.util.*;
+
+
+
+
+class Audio{
+
+//    private String fileName = "resources/gson/8-bit-audio";
+//    String uriString = new File(fileName).toURI().toString();
+//    MediaPlayer player;
+//
+//    {
+//        player = new MediaPlayer(new Media(uriString));
+//    }
+//    player.play(); // or stop() or pause() etc etc
+//
+//    void playMP3(String fileName) {
+//        new javafx.embed.swing.JFXPanel();
+//        String uriString = new File(fileName).toURI().toString();
+//        new MediaPlayer(new Media(uriString) {
+//            @Override
+//            public void addMarker(String s, double v) {
+//
+//            }
+//
+//            @Override
+//            public double removeMarker(String s) {
+//                return 0;
+//            }
+//
+//            @Override
+//            public void removeAllMarkers() {
+//
+//            }
+//
+//            @Override
+//            public Map<String, Double> getMarkers() {
+//                return null;
+//            }
+//        }).play();
+//    }
+
+}
